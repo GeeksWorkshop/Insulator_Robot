d1cd4b66227005a78db35650ae4db2b4232583f7
oym1994 <448868809@qq.com>
2018-02-02 20:59:02 +0800

    init

---
+++ Insulator_Robot/USER/main.c
@@ -0,0 +1,40 @@
+#include "led.h"
+#include "delay.h"
+#include "sys.h"
+#include "usart.h"
+#include "dma.h"
+#include "timer.h"
+
+   	
+extern RC_Ctl_t RC_CtrlData;
+int speed=0;
+
+ int main(void)
+ { 
+
+	delay_init();	    	
+	LED_Init();		  		
+  TIM2_PWM_Init(999,1439);	
+	uart_init(100000);	 
+  delay_ms(1000);
+  USART1_DMA_RX();
+  TIM_SetCompare2(TIM2,800);
+ 
+	 while(1)
+		 
+	 {
+	 speed=925+(RC_CtrlData.rc.ch3-1024)*0.03;
+	 TIM_SetCompare2(TIM2,speed);
+	 TIM_SetCompare3(TIM2,speed);
+	 TIM_SetCompare4(TIM2,speed);
+
+	 GPIO_SetBits(GPIOC, GPIO_Pin_7); 
+	 GPIO_ResetBits(GPIOC, GPIO_Pin_8);
+	 delay_ms(100);
+	 GPIO_SetBits(GPIOC, GPIO_Pin_8); 
+	 GPIO_ResetBits(GPIOC, GPIO_Pin_7);
+	 delay_ms(100);
+
+	 }
+
+}
